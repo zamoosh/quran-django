072964ab8ef3b33e05829504a39955ed86a49259
Mohammad Rahimi <moyi.pary@gmail.com>
2023-02-14 01:27:19 +0330

    fix database name

--- quran/models.py
+++ quran/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 
-class QuranText(models.Model):
-    ayah_id = models.AutoField(primary_key=True)  # is unique for every ayah
-    surah_id = models.IntegerField()
-    ayah = models.IntegerField()
+class Text(models.Model):
+    index = models.AutoField(primary_key=True)
+    sura = models.IntegerField()
+    aya = models.IntegerField()
     text = models.TextField()
